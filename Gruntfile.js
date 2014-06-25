1076935985727a2ed39e977b5368205252de0237
Evan Shortiss <evan.shortiss@feedhenry.com>
2014-06-25 09:43:27 +0100

    Build updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,68 +7,82 @@ module.exports = function(grunt) {
 
 
     watch: {
-      files: ['./www/**/*', '!./www/js/bundle.js',
-        '!./www/bower_components/**'],
-      tasks: ['build'],
+      files: [
+        './www_dev/**/*',
+        // Don't watch deps or generated files
+        '!./www_dev/js/bundle.js',
+        '!./www_dev/js/bundle.css',
+        '!./www_dev/bower_components/**'
+      ],
+      tasks: ['build:debug'],
       options: {
         'event': ['all']
       }
     },
 
+
     browserSync: {
       dev: {
         bsFiles: {
-          src : 'www/**'
+          src : 'www_dev/**'
         },
         options: {
           watchTask: true,
           server: {
-            baseDir: './www'
+            baseDir: './www_dev'
           }
         }
       }
     },
 
-    // TODO: Investgiate why transform fails...using grunt-shel instead for now
-    // browserify: {
-    //   debug: {
-    //     files: {
-    //       './www/js/bundle.js': ['./www/**/*.js', '!./www/js/bundle.js'],
-    //     },
-    //     options: {
-    //       bundleOptions: {
-    //         // 'debug': true,
-    //         'entry': './www/js/app.js',
-    //         'transform': ['brfs'],
-    //       }
-    //     }
-    //   }
-    // },
 
     shell: {
       'bower-install': {
         command: 'bower install'
       },
       'browserify-debug': {
-        command: 'browserify ./www/js/**/*.js -t brfs -e ./www/js/app.js ' +
-          '-o www/js/bundle.js -d'
+        command: 'browserify ./www_dev/js/**/*.js -t brfs -e ./www_dev/js/app.js ' +
+          '-o ./www_dev/bundle.js -d'
       },
-      'browserify-dist': {
-        command: 'browserify ./www/js/**/*.js -t brfs -e ./www/js/app.js ' +
-          '-o www/js/bundle.js'
+      'browserify-release': {
+        command: 'browserify ./www_dev/js/**/*.js -t brfs -e ./www_dev/js/app.js ' +
+          '-o ./www/bundle.js'
       }
     },
+
 
     column_lint: {
       files: {
-        src: ['./www/**/*.js', '!./www/js/bundle.js']
+        src: ['./www_dev/**/*.js', '!./www_dev/js/bundle.js']
       }
     },
 
+
+    dom_munger: {
+      release: {
+        options: {
+          src: './www_dev/index.html',
+          read: [{
+            selector: 'link',
+            attribute: 'href',
+            writeto: 'cssFiles',
+            isPath: true
+          }, {
+            selector: 'script[src]',
+            attribute: 'src',
+            writeto: 'jsFiles',
+            isPath: true
+          }]
+        }
+      }
+    },
+
+
     wiredep: {
-      debug: {
+      //
+      all: {
         src: [
-          './www/index.html'
+          './www_dev/index.html'
         ],
 
         dependencies: true,
@@ -77,15 +91,41 @@ module.exports = function(grunt) {
     },
 
     jshint: {
-      src: ['Gruntfile.js', './www/**/*.js', '!./www/js/bundle.js'],
+      src: ['Gruntfile.js', './www_dev/**/*.js', '!./www_dev/js/bundle.js'],
       options: {
         jshintrc: './jshintrc.js'
       }
     },
 
+
+    cssmin: {
+      // Combine our own CSS files for debug builds
+      combine: {
+        files: {
+          './www_dev/bundle.css': ['./www_dev/css/**/*.css']
+        }
+      },
+      // Minify bower and our own CSS for release
+      minify: {
+        expand: true,
+        src: ['<%= dom_munger.data.cssFiles %>', './www_dev/css/**/*.css'],
+        dest: './www/bundle.css'
+      }
+    },
+
+
+    uglify: {
+      // Create release JS from script tags and browserified JS bundle
+      release: {
+        src:['<%= dom_munger.data.jsFiles %>', './www_dev/bundle.js'],
+        dest: './www/bundle.js'
+      }
+    },
+
+
     lintspaces: {
       javascript: {
-        src: ['./www/**/*.js', '!./www/js/bundle.js'],
+        src: ['./www_dev/**/*.js', '!./www_dev/js/bundle.js'],
         options: {
           // TODO: Reference editorconfig
           indentation: 'spaces',
@@ -96,6 +136,27 @@ module.exports = function(grunt) {
         }
       }
     },
+
+
+    copy: {
+      release: {
+        files: [{
+          src: [
+            // Copy everything...
+            './www_dev/**',
+            // ... but don't copy these files as they'll be bundled
+            '!./www_dev/js/',
+            '!./www_dev/css/',
+            '!./www_dev/bundle.js',
+            '!./www_dev/bundle.css',
+            '!./www_dev/bower_components/'
+          ],
+          dest: './www/',
+          expand: true
+        }]
+      }
+    },
+
 
     karma: {
       browsers: {
@@ -108,16 +169,42 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('grunt-dom-munger');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-lintspaces');
   grunt.loadNpmTasks('grunt-column-lint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-browser-sync');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask('test', ['build', 'karma']);
-  grunt.registerTask('build', ['shell:bower-install', 'shell:browserify-debug',
-    'wiredep:debug']);
-  grunt.registerTask('format', ['lintspaces', 'jshint']);
-  grunt.registerTask('serve', ['browserSync', 'watch']);
+
+  // Run all unit tests
+  grunt.registerTask('test', ['build:debug', 'karma']);
+
+  // Code quality checks
+  grunt.registerTask('format', ['lintspaces', 'jshint', 'column_lint']);
+
+  // Serve files and watch for changes
+  grunt.registerTask('serve', ['build:debug', 'browserSync', 'watch']);
+
+  // Build debug files for ./www
+  grunt.registerTask('build:debug', [
+    'shell:bower-install',
+    'shell:browserify-debug',
+    'wiredep:all',
+    'cssmin:combine'
+  ]);
+
+  // Build release files and write to /www
+  grunt.registerTask('build:release', [
+    'build:debug', // Debug src needs to be configured first
+    'shell:browserify-release',
+    'dom_munger:release',
+    'uglify:release',
+    'cssmin:minify',
+    'copy:release'
+  ]);
 };
